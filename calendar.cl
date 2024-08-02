d2249e657a3b6b237b63bfad09e669168ada65fb
dnbias <daniel.biasiotto@edu.unito.it>
2024-08-02 23:27:40 +0200

    pretty print and some implementation

--- calendar.cl
+++ calendar.cl
@@ -1,18 +1,18 @@
 % clingo src file
 % an academic calendar solver
 
-week(1..1).
+week(1..3).
 day(mon;tue;wed;thu;fri;sat).
 hour(1..8).
+fulltime(3).
 hours(W,D,0):-week(W), not fulltime(W), day(D), D!=fri, D!=sat.
 hours(W,D,8):-week(W), fulltime(W), day(D), D!=fri, D!=sat.
 hours(W,fri,8):-week(W).
 hours(W,sat,6):-week(W).
-fulltime(2).
 
 teacher(muzzetto;pozzato;gena;tomatis;micalizio).
 subject(pres;free;pm;fictpp;lm;gq;aslcweb).
-hoursOf(free,2;pm,2;fictpp,2;lm,2;gq,2;aslcweb,2).
+hoursOf(pres,2;free,2;pm,2;fictpp,4;lm,2;gq,2;aslcweb,2).
 hoursCount(S,N):- subject(S), N = #count { W,D,H,S: assign(W,D,H,S), subject(S) }.
 
 teaching(muzzetto,pm).
@@ -21,6 +21,9 @@ teaching(gena,lm).
 teaching(tomatis,gq).
 teaching(micalizio,aslcweb).
 
+prerequisite(fictpp,aslcweb).
+
+% presentation is first hours of the course
 assign(1,fri,1,pres).
 assign(1,fri,2,pres).
 
@@ -35,11 +38,22 @@ assign(1,fri,2,pres).
   teaching(T,S), teaching(T,S1), teaching(T,S2),
   H!=H1,H!=H2,H1!=H2, H\2==1, H1\2==1, H2\2==1.
 % max 2 slots per subject per day
-:-assign(W,D,H,S), assign(W,D,H1,S1), assign(W,D,H2,S2),
+:-S!=free, assign(W,D,H,S), assign(W,D,H1,S1), assign(W,D,H2,S2),
   S==S1,S==S2,S1==S2,
   H!=H1,H!=H2,H1!=H2, H\2==1, H1\2==1, H2\2==1.
 % hours of teaching must be fullfilled
-:-hoursCount(S,N), hoursOf(S,N1), N!=N1.
+:-S!=free, hoursCount(S,N), hoursOf(S,N1), N!=N1.
+:-S==free, hoursCount(S,N), hoursOf(S,N1), N<N1.
+% prerequisites must be respected TODO
+% last lesson of left-side must precede first lesson of right-side
+:-assign(W,D,H,S), assign(W1,D1,H1,S), assign(W2,D2,H2,S1),
+  prerequisite(S,S1), W>=W1, D>=D1, H>=H1,
+  W2<W, D2<D, H2<H.
+% distance between start and end of any teaching cannot exceed 6 weeks
+:-assign(W,D,H,S), assign(W1,D1,H1,S),
+  assign(W2,D2,H2,S), assign(W3,D3,H3,S),
+  W<=W1, D<=D1, H<=H1, W3>=W2, D3>=D2, H3>=H2, W3-W>=6.
+
 
 #show assign/4.
 #show hoursCount/2.
---
+++ pretty-print.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+import re
+import sys
+import fileinput
+import itertools
+from tabulate import tabulate
+from enum import StrEnum, auto
+
+name_of_script = sys.argv[0]
+input = ''
+for line in fileinput.input(encoding="utf-8"):
+    input += line
+
+calendar = []
+hoursCount = []
+
+class Day(StrEnum):
+    MON = auto()
+    TUE = auto()
+    WED = auto()
+    THU = auto()
+    FRI = auto()
+    SAT = auto()
+    def __lt__(self, other: 'Day'):
+        if self == other:
+            return False
+        # the following works because the order of elements in the definition is preserved
+        for elem in Day:
+            if self == elem:
+                return True
+            elif other == elem:
+                return False
+        raise RuntimeError('Bug: we should never arrive here')  # I just like being pedantic
+
+    def __gt__(self, other):
+        return not (self < other)
+
+    def __ge__(self, other):
+        if self == other:
+            return True
+        return not (self < other)
+
+
+
+class MetaAssignment(type):
+    def __iter__(self):
+        for attr in dir(self):
+            if not attr.startswith("__"):
+                yield attr
+
+class Assignment(metaclass=MetaAssignment):
+    def __str__(self):
+        return f"{self.week},{self.day},{self.hour},{self.sub}"
+
+    week = 0
+    day = Day('mon')
+    hour = 0
+    sub = ''
+
+def pprint(clingo_str):
+    # print(clingo_str)
+    tokens = clingo_str.split()
+    for token in tokens:
+        if 'assign' in token:
+            m = re.search(r"\(([A-Za-z0-9,_]+)\)", token)
+            args = m.group(1).split(',')
+            a = Assignment()
+            a.week = args[0]
+            a.day = Day(args[1])
+            a.hour = args[2]
+            a.sub = args[3]
+            # updateCalendar(a)
+            calendar.append(a)
+        elif 'hoursCount' in token:
+            m = re.search(r"\(([A-Za-z0-9,_]+)\)", token)
+            args = m.group(1).split(',')
+            hoursCount.append(f"{args[0]}:{args[1]}")
+
+    for h in hoursCount:
+        print(h)
+
+    calendar.sort(key=lambda x: (x.week, x.day, x.hour))
+    key_fun = lambda x: x.week
+    weeks_calendar = [list(group) for key, group in itertools.groupby(calendar, key_fun)]
+
+    for w in weeks_calendar:
+        key_fun = lambda x: x.day
+        days_calendar = [list(group) for key, group in itertools.groupby(w, key_fun)]
+        print('----------------------------------------------------------------------')
+        print(f'week {w[0].week}')
+
+        for d in days_calendar:
+            d.sort(key=lambda x: (x.day, x.hour))
+            print()
+            print(d[0].day)
+            for attr in ('hour', 'sub'):
+                print(' '.join('%-8s'%getattr(item, attr) for item in d))
+
+
+
+        # tab = tabulate(w, headers[""])
+        # print(tab)
+        # key_fun = lambda x: x.day
+        # days = [list(group) for key, group in itertools.groupby(w, key_fun)]
+        # for d in days:
+        # print(f'{d[0].day}')
+
+pprint(input)
